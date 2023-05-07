8dc1ce99e7f582f2667cf3ae5891c681f704b0cd
anvaka <anvaka@gmail.com>
2023-05-07 12:25:30 -0700

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '',
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

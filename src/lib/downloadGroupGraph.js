f3d0f8542e953651e98d23425f93018f0ab7238c
anvaka <anvaka@gmail.com>
2025-05-20 00:43:57 -0700

    avoid duplicate fetches

--- src/lib/downloadGroupGraph.js
+++ src/lib/downloadGroupGraph.js
@@ -1,14 +1,32 @@
-// import createGraph from 'ngraph.graph';
 import config from './config';
 
 const graphsCache = new Map();
+const pendingRequests = new Map();
 
 export default async function downloadGroupGraph(groupId) {
   if (graphsCache.has(groupId)) {
     return graphsCache.get(groupId);
   }
-  // let graph = createGraph();
 
+  // Prevent duplicate network requests by returning the in-flight promise
+  if (pendingRequests.has(groupId)) {
+    return pendingRequests.get(groupId);
+  }
+
+  const promise = fetchAndProcessGraph(groupId);
+  pendingRequests.set(groupId, promise);
+  
+  try {
+    const graph = await promise;
+    graphsCache.set(groupId, graph);
+    return graph;
+  } finally {
+    // Always clean up to prevent memory leaks, even on errors
+    pendingRequests.delete(groupId);
+  }
+}
+
+async function fetchAndProcessGraph(groupId) {
   let response = await fetch(`${config.graphsEndpoint}/${groupId}.graph.dot`);
   let text = await response.text();
 
@@ -16,29 +34,7 @@ export default async function downloadGroupGraph(groupId) {
   let graph = fromDot.default(text);
   graph.forEachNode(node => {
     node.data.l = node.data.l.split(',').map(x => +x);
-  })
-  // let nodeFetch = fetch(`${config.graphsEndpoint}/${groupId}.nodes.json`).then(r => r.json()).then(nodePositions => {
-  //   let nodeId = 0;
-  //   for (let i = 0; i < nodePositions.length; i += 2) {
-  //     nodeId += 1;
-  //     graph.addNode(nodeId, [nodePositions[i], nodePositions[i + 1]]);
-  //   }
-  // });
-  // let linkFetch = fetch(`${config.graphsEndpoint}/${groupId}.links.bin`).then(r => r.arrayBuffer()).then(buffer => {
-  //   // buffer is int32 array:
-  //   let view = new Int32Array(buffer);
-  //   let lastFrom = 0;
-  //   for (let i = 0; i < view.length; i++) {
-  //     let v = view[i];
-  //     if (v < 0) {
-  //       // new source node
-  //       lastFrom = -v;
-  //       continue;
-  //     }
-  //     graph.addLink(lastFrom, v);
-  //   }
-  // });
-  // await Promise.all([nodeFetch, linkFetch]);
-  graphsCache.set(groupId, graph);
+  });
+
   return graph;
 }
